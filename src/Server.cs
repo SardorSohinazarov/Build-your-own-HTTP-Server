e21998bd9ff8085e453f756a8d06b5d51681b984
SardorSohinazarov <sardorstudent0618@gmail.com>
2025-04-26 19:37:24 +0500

    200

--- src/Server.cs
+++ src/Server.cs
@@ -1,10 +1,19 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
-server.Start();
-server.AcceptSocket(); // wait for client
+server.Start(); // serverni boshlaydi, ya'ni soket ochiladi va eshitishni boshlaydi.
+Console.WriteLine("4221 eshitish boshlandi!");
+var clientSocket = server.AcceptSocket(); // bu metod kliyent ulanmaguncha kutadi. Kimdir ulanadi, shunda bu metod ulanishni qabul qiladi va Socket obyektini qaytaradi.
+Console.WriteLine("Client ulandi");
+
+var response = "HTTP/1.1 201 Created\r\n";
+byte[] responseBytes = Encoding.UTF8.GetBytes(response); // bu metod serverdan kliyentga HTTP javobini yuboradi.
+clientSocket.Send(responseBytes); // bu metod kliyentga javob yuboradi.
+clientSocket.Close(); // bu metod kliyentni yopadi.
+server.Stop(); // serverni to'xtatadi, ya'ni soketni yopadi.
